cc908e8d16ce31aaad6bde89e4e1955dca9278e8
shashirl9 <shashidhar.lucky9@gmail.com>
2025-02-03 18:30:24 +1100

    pandagala havaliclear

---
+++ src/main/java/com/shashi/learn/pgm1a_hello_world/Pgm1aHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.shashi.learn.pgm1a_hello_world;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pgm1aHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pgm1aHelloWorldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shashi/learn/pgm1a_hello_world/Pgm1aHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shashi.learn.pgm1a_hello_world;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pgm1aHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
